a11df7ff1547843924d8191dcdaaaf4b911726fc
polyca-git <egitim.foolvictim@gmail.com>
2021-06-03 00:20:18 +0200

    Another, short way of creating arrow functions.

--- play.js
+++ play.js
@@ -7,9 +7,8 @@ const summarizeUser = (userName, userAge, userHasHobby) => {
     return ('Name is '+userName+', age is '+userAge+' and the user has hobbies: '+ userHasHobby);
 }; //function kelimesini silip parametrelerden sonra => ekledik, arrow functionlar ile this keyword unu kullanabiliyoruz.
 
-const add =(a,b)=>{
-    return a+b;
-};
+const add =(a,b)=> a + b ; //return u sildik, bracelet i sildik ve fonksiyonumuzu böyle tanımladık.
+
 console.log(add(1,2));
 
 console.log(summarizeUser(name,age,hasHobbies));
