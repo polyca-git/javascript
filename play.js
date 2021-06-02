d0e92638b726230943ddb1114f63fb61cc87255a
polyca-git <egitim.foolvictim@gmail.com>
2021-06-03 00:27:35 +0200

    Creating an object

--- play.js
+++ play.js
@@ -1,12 +1,6 @@
+const person = {
+name: 'Max',
+age: 29
+};
 
-const name= 'Max';
-let age=29;
-const hasHobbies=true;
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-    return ('Name is '+userName+', age is '+userAge+' and the user has hobbies: '+ userHasHobby);
-}; //function kelimesini silip parametrelerden sonra => ekledik, arrow functionlar ile this keyword unu kullanabiliyoruz.
-
-const addRandom = () => 1 + 2; //iff arrow function don't have any paramters, we should place a "()".
-console.log(addRandom());
-console.log(summarizeUser(name,age,hasHobbies));
+console.log(person);
