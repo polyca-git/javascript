265961ea76e15ebd75bc49a83260ac00ad77c1e3
polyca-git <egitim.foolvictim@gmail.com>
2021-06-03 00:23:03 +0200

    More shorter way to create an arrow function if the function only has 1 parameter

--- play.js
+++ play.js
@@ -7,8 +7,7 @@ const summarizeUser = (userName, userAge, userHasHobby) => {
     return ('Name is '+userName+', age is '+userAge+' and the user has hobbies: '+ userHasHobby);
 }; //function kelimesini silip parametrelerden sonra => ekledik, arrow functionlar ile this keyword unu kullanabiliyoruz.
 
-const add =(a,b)=> a + b ; //return u sildik, bracelet i sildik ve fonksiyonumuzu böyle tanımladık.
-
-console.log(add(1,2));
+const addOne = a => a + 1; //Tek parametre yollayaağımız için parametre tanımlarken parantezi kaldırdık.
+console.log(addOne(1));
 
 console.log(summarizeUser(name,age,hasHobbies));
