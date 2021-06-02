05cb1450aeb8b0ce3ffd6c77082ee4db6df0ac54
polyca-git <egitim.foolvictim@gmail.com>
2021-06-03 00:31:32 +0200

    Placing function inside of the objects and using them

--- play.js
+++ play.js
@@ -1,6 +1,9 @@
 const person = {
 name: 'Max',
-age: 29
+age: 29,
+greet: () =>{
+    console.log('Hi, I am '+this.name);
+} //placing an arrow function inside of the object and using the object's variable with this keyword.
 };
 
-console.log(person);
+person.greet(); //When you run the code it says Hi, I am undefined, we will solve this at the next commits.
