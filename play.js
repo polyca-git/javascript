5d718aa7beb587979661bd131d2ca7769961048a
polyca-git <egitim.foolvictim@gmail.com>
2021-06-03 00:35:57 +0200

    Another way of accessing variables inside of the object with 'this' keyword.

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const person = {
 name: 'Max',
 age: 29,
-greet: function() { //removing arrow function and using the old way is a solution 
+greet() { //We can also directly define the function as another solution.
     console.log('Hi, I am '+this.name);
 } //placing an arrow function inside of the object and using the object's variable with this keyword.
 };
