c929256c2fc99ab4f8eb2fa59434f59a0ebfd040
polyca-git <egitim.foolvictim@gmail.com>
2021-06-03 00:34:12 +0200

    One of the correct ways to access variables inside of the object with 'this' keyword.

--- play.js
+++ play.js
@@ -1,9 +1,9 @@
 const person = {
 name: 'Max',
 age: 29,
-greet: () =>{
+greet: function() { //removing arrow function and using the old way is a solution 
     console.log('Hi, I am '+this.name);
 } //placing an arrow function inside of the object and using the object's variable with this keyword.
 };
 
-person.greet(); //When you run the code it says Hi, I am undefined, we will solve this at the next commits.
+person.greet(); 
