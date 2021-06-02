76fd126a5f98809b212c29d3f8b3d5d8b727b15f
polyca-git <egitim.foolvictim@gmail.com>
2021-06-03 00:18:15 +0200

    Using of arrow functions

--- play.js
+++ play.js
@@ -3,11 +3,13 @@ const name= 'Max';
 let age=29;
 const hasHobbies=true;
 
-const summarizeUser = function (userName, userAge, userHasHobby){
+const summarizeUser = (userName, userAge, userHasHobby) => {
     return ('Name is '+userName+', age is '+userAge+' and the user has hobbies: '+ userHasHobby);
-}
+}; //function kelimesini silip parametrelerden sonra => ekledik, arrow functionlar ile this keyword unu kullanabiliyoruz.
+
+const add =(a,b)=>{
+    return a+b;
+};
+console.log(add(1,2));
 
 console.log(summarizeUser(name,age,hasHobbies));
-
-//var ile değişken tanımlanır, eski bir syntaxtır. efendiler, let kullanacağız.
-//eğer bir değişkeni değiştirmeyeceğimize eminsek const kullanacağız.
