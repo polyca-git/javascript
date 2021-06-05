5fbc6d0cd3c55d84514e067deba797a8409bb065
polyca-git <egitim.foolvictim@gmail.com>
2021-06-05 22:03:03 +0200

    Destructuring over objcts and arrays

--- play.js
+++ play.js
@@ -5,7 +5,24 @@ greet() { //We can also directly define the function as another solution.
     console.log('Hi, I am '+this.name);
 } //placing an arrow function inside of the object and using the object's variable with this keyword.
 };
+const printName = ({name}) => { //With the curly bracelets{} inside of paranthesis we can customize whic data we want. This is called Destructuring.
+    console.log(name);
+}
 
+printName(person);
+
+const {name, age} = person; //By doing destructuring, we took name and age properties from person object, we can use them here with the same name.
+console.log(name, age);
+
+const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies; //We can destructure arrays too. This way we take first 2 elements of the array to variables we named.
+console.log(hobby1, hobby2);
+
+//While doing object destructuring, we should specify the key names (like name, age etc.) but while doing array destructuring
+//we can hive any name we want, because arrays dont have key names for their values.
+
+
+/*
 const hobbies = ['Sports', 'Cooking']; //Declaration of Arrays
 //console.log(hobbies.map(hobby=> 'Hobby: '+hobby)); //shorter way of using map functions over arrays.
 //console.log(hobbies);
@@ -19,3 +36,4 @@ const toArray = (...args)=>{ //This is called the rest operator, takes all the a
 };
 
 console.log(toArray(1, 2, 3));
+*/
